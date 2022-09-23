1d73a023f7b7a5a495c392b876413583305f3206
Irlanda Maldonado <irly_malvel@hotmail.com>
2022-09-22 18:17:04 -0700

    Agregar archivos de proyecto.

---
+++ Practica_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practica_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroLados;
+            double tamanoLado, perimetro;
+            Console.WriteLine("Dame el numero de lados");
+            numeroLados = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dame la medida de uno de los lados");
+            tamanoLado = Convert.ToDouble(Console.ReadLine());
+
+            perimetro = numeroLados * tamanoLado;
+
+            Console.WriteLine("El perimetro es:{0}", perimetro);
+
+        }
+    }
+}
